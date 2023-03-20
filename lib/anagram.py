c48fd2c700d9f1adf939462ae385c8805120a226
Eric Thvedt <ericsthvedt@gmail.com>
2023-03-19 22:06:51 -0700

    passed tests

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, list):
+        matched_words = []
+        for test_word in list:
+            if sorted(test_word) == sorted(self.word):
+                matched_words.append(test_word)
+        return matched_words
